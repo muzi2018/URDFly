9226bb3e933df7acc504007a7b344c00e82f217b
Weijie Wang <duangsir518@163.com>
2025-10-20 18:45:21 +0200

    update urdf

--- urdf_parser_JL.py
+++ urdf_parser_JL.py
@@ -16,14 +16,20 @@ def count_joints_and_links(urdf_file_path):
     # Collect joint names
     joint_names = [joint.get("name") for joint in joints]
 
-    return num_links, num_joints, joint_names
+    # Collect link names
+    link_names = [link.get("name") for link in links]
+
+    return num_links, num_joints, joint_names, link_names
 
 
 if __name__ == "__main__":
-    urdf_path = "/home/wang/URDFly/descriptions/urdf0924/urdf/urdf0924.urdf"
-    links, joints, joint_names = count_joints_and_links(urdf_path)
+    urdf_path = "/home/wang/URDFly/descriptions/kyon/kyon.urdf"
+    links, joints, joint_names, link_names = count_joints_and_links(urdf_path)
     print(f"Number of links: {links}")
     print(f"Number of joints: {joints}")
     print("Joint names:")
     for name in joint_names:
+        print(f" - {name}")
+    print("Link names:")
+    for name in link_names:
         print(f" - {name}")
